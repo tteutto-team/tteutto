8af17945f150f1a613b55a4577b5f1253639614c
wooheesu <hsoo321@naver.com>
2022-01-25 21:35:23 +0900

    main 컨트롤러 수정

--- src/main/java/edu/kh/tteutto/main/controller/MainContoller.java
+++ src/main/java/edu/kh/tteutto/main/controller/MainContoller.java
@@ -6,13 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/main/*")
 public class MainContoller {
-
-	// 메인 페이지
-	@RequestMapping("tteutto")
-	public String mainPage() {
-		return "main/main";
-	}
-	
 	
 	// 클래스 검색 목록
 	@RequestMapping("searchList")
