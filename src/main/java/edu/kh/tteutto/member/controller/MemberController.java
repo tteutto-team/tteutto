aba0be410264b8467568280e975b3073b58217af
sjoon_seo <seosj0319@gmail.com>
2022-01-26 13:06:00 +0900

    커밋

--- src/main/java/edu/kh/tteutto/member/controller/MemberController.java
+++ src/main/java/edu/kh/tteutto/member/controller/MemberController.java
@@ -19,16 +19,4 @@ public class MemberController {
 	public String login() {
 		return "member/login";
 	}
-	
-	// 학생 프로필 페이지 이동
-	@RequestMapping(value="studentProfile")
-	public String studentProfile() {
-		return "member/studentProfile";
-	}
-	
-	// 강사 프로필 페이지 이동
-	@RequestMapping(value="teacherProfile")
-	public String teacherProfile() {
-		return "member/teacherProfile";
-	}
 }
