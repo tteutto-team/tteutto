78a4e449b03b1ecebedc56b7c42560e473800a6a
sjoon_seo <seosj0319@gmail.com>
2022-01-28 12:03:41 +0900

    백앤드 시작

---
+++ src/main/java/edu/kh/tteutto/member/model/dao/MemberDAO.java
@@ -0,0 +1,25 @@
+package edu.kh.tteutto.member.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.tteutto.member.model.vo.Member;
+
+@Repository
+public class MemberDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	/** 로그인
+	 * @param memberEmail
+	 * @return loginMember
+	 */
+	public Member login(String memberEmail) {
+		
+		System.out.println(memberEmail);
+		return sqlSession.selectOne("memberMapper.login", memberEmail);
+	}
+
+}
