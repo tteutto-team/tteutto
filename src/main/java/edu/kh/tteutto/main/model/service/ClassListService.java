9946f0c3feb7eb2790eefe2c65d67191092a3943
haramco <layssingcar@daum.net>
2022-02-09 12:09:12 +0900

    클래스 개수, 카드 조회 mapper 합치기 진행 중 / 클래스 카드 css 수정

--- src/main/java/edu/kh/tteutto/main/model/service/ClassListService.java
+++ src/main/java/edu/kh/tteutto/main/model/service/ClassListService.java
@@ -8,25 +8,29 @@ import edu.kh.tteutto.main.model.vo.Pagination;
 
 public interface ClassListService {
 	
-	/** 신규 클래스 추천 목록 조회
-	 * @param memberNo
-	 * @return newList
-	 */
-	List<ClassList> selectNewList(int memberNo);
-	
-	/** 클래스 개수 조회 + 페이지네이션
-	 * @param search
+	/** 클래스 개수 조회
+	 * @param map
 	 * @param page
 	 * @return pagination
 	 */
-	Pagination getPagination(String search, int page);
+	Pagination getPagination(Map<String, Object> map, int page);
 	
-	/** 클래스 카드 목록 조회
+	/** 클래스 카드 조회
 	 * @param pagination
 	 * @param map
 	 * @return searchList
 	 */
 	List<ClassList> selectSearchList(Pagination pagination, Map<String, Object> map);
+	
+	
+	
+	
+	
+	/** 신규 클래스 추천 목록 조회
+	 * @param memberNo
+	 * @return newList
+	 */
+	List<ClassList> selectNewList(int memberNo);
 
 	/** 클래스 추천 목록 조회
 	 * @param memberNo
